6bd07a36ed0e0fd69abc45c43182791a271ada61
Magnusson95 <alexander.magnusson95@gmail.com>
2020-11-08 16:28:37 +0000

    Customised Admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,36 @@ from django.contrib import admin
 from .models import Animal, Category, Service
 
 # Register your models here.
-admin.site.register(Animal)
-admin.site.register(Category)
-admin.site.register(Service)
+
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = (
+        'animal',
+        'category'
+    )
+
+    ordering = ('category', 'animal',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'fee_multiplier'
+    )
+
+    ordering = ('fee_multiplier',)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'service_type',
+        'sub_heading',
+        'cost'
+    )
+
+    ordering = ('service_type',)
+
+
+admin.site.register(Animal, AnimalAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Service, ServiceAdmin)
