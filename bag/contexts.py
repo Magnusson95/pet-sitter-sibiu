96e26432f8e832d62e78651a0b930afca5220ecb
Magnusson95 <alexander.magnusson95@gmail.com>
2020-11-22 10:34:18 +0000

    Updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,15 +1,28 @@
+from django.shortcuts import get_object_or_404
+from products.models import Service
 
 
 def bag_contents(request):
 
     bag_items = []
-    grand_total = 0
-    product_count = 0
+    total = 0
+    service_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        service = get_object_or_404(Service, pk=item_id)
+        total += quantity * service.cost
+        service_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service,
+        })
 
     context = {
         'bag_items': bag_items,
-        'grand_total': grand_total,
-        'product_count': product_count,
+        'total': total,
+        'service_count': service_count,
     }
 
     return context
