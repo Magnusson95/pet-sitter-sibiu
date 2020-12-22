8bfd0f980ed4098f5b84a3a1a7ef1229d8f51118
Magnusson95 <alexander.magnusson95@gmail.com>
2020-12-22 14:14:45 +0000

    Added pagination to collage

--- collage/views.py
+++ collage/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from collage.models import Collage
 # Create your views here.
 
@@ -7,7 +8,14 @@ def view_collage(request):
     """ A view to return the collage page """
     photos = Collage.objects.all()
 
+    photo_paginator = Paginator(photos, 20)
+
+    page_num = request.GET.get('page')
+
+    page = photo_paginator.get_page(page_num)
+
     context = {
-        'photos': photos,
+        'count': photo_paginator.count,
+        'page': page
     }
     return render(request, 'collage/collage.html', context)
