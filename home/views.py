0478f162f296a57be33b5d7c41955d6002caa216
Magnusson95 <alexander.magnusson95@gmail.com>
2021-03-30 21:51:49 +0000

    Rendered reviews in home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from products.models import Service
+from reviews.models import UserReview
 
 # Create your views here.
 
@@ -8,8 +9,10 @@ def index(request):
     """ A view to return the index page """
 
     services = Service.objects.all()
+    reviews = UserReview.objects.all()
 
     context = {
         'services': services,
+        'reviews': reviews,
     }
     return render(request, 'home/index.html', context)
