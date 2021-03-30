dead6a5b62d03b678b85c3e422f4ae81bd0f1a04
Magnusson95 <alexander.magnusson95@gmail.com>
2021-03-30 17:04:19 +0000

    Edit product view added

--- products/views.py
+++ products/views.py
@@ -42,7 +42,7 @@ def add_service(request):
             messages.success(request, 'Successfully added service')
             return redirect(reverse('add_service'))
         else:
-            messages.error(request, 'Failed to add servce, ense form is valid')
+            messages.error(request, 'Failed to add servce, please check if form is valid')
     else:
         form = ServiceForm()
     template = 'products/add_service.html'
@@ -51,3 +51,27 @@ def add_service(request):
     }
 
     return render(request, template, context)
+
+
+def edit_service(request, service_id):
+    """ Edit an existing service on the site """
+    service = get_object_or_404(Service, pk=service_id)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES, instance=service)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated service!')
+            return redirect(reverse('service_detail', args=[service.id]))
+        else:
+            messages.error(request, 'Failed to update service. Please ensure valid form.')
+    else:
+        form = ServiceForm(instance=service)
+        messages.info(request, f'You are editing {service.service_type}')
+
+        template = 'products/edit_service.html'
+        context = {
+            'form': form,
+            'service': service
+        }
+
+        return render(request, template, context)
