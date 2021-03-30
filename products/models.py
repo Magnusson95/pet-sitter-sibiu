284801f012c48e49424f72421e5e00718259ddad
Magnusson95 <alexander.magnusson95@gmail.com>
2021-03-30 15:58:15 +0000

    Added add_service post method. Added profile update error message. Fixed noimage bug in toasts and bag

--- products/models.py
+++ products/models.py
@@ -26,7 +26,7 @@ class Animal(models.Model):
 class Service(models.Model):
     service_type = models.CharField(max_length=254)
     period = models.DecimalField(max_digits=6, decimal_places=0, validators=[MinValueValidator(0)])
-    cost = models.DecimalField(max_digits=6, decimal_places=0)
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
     sub_heading = models.CharField(max_length=254)
     detail_1 = models.CharField(max_length=254)
     detail_2 = models.CharField(max_length=254)
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Service, Animal, Category
 from .forms import ServiceForm
 # Create your views here.
@@ -34,7 +35,16 @@ def service_detail(request, service_id):
 
 def add_service(request):
     """ Add a new service to the site """
-    form = ServiceForm()
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added service')
+            return redirect(reverse('add_service'))
+        else:
+            messages.error(request, 'Failed to add servce, ense form is valid')
+    else:
+        form = ServiceForm()
     template = 'products/add_service.html'
     context = {
         'form': form,
