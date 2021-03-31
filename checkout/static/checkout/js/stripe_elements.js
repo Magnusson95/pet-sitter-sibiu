e8144216cf34147fd27a9c57b60528809348e7b1
Magnusson95 <alexander.magnusson95@gmail.com>
2021-03-31 02:32:08 +0000

    Stripe post code fix

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -26,7 +26,7 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {hidePostalCode: true, style: style});
+var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
