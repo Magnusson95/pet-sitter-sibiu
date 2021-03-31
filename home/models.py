a8596ffb5d2ec06549e17c0d3117619ae25c0236
Magnusson95 <alexander.magnusson95@gmail.com>
2021-03-31 11:13:22 +0000

    Removed email functionality

--- home/models.py
+++ home/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 
-
-class EmailForm(models.Model):
-    """
-    Model for sending emails
-    """
-    name = models.CharField(max_length=50, null=False, blank=False)
-    message = models.TextField()
-    email = models.EmailField(max_length=254, null=False, blank=False)
-
-    def __str__(self):
-        return self.name
--- home/views.py
+++ home/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from products.models import Service
-from django.core.mail import send_mail
-from django.contrib import messages
-from django.conf import settings
 from reviews.models import UserReview
-from .models import EmailForm
 
 # Create your views here.
 
@@ -12,49 +8,11 @@ from .models import EmailForm
 def index(request):
     """ A view to return the index page """
 
-    if request == "POST":
-        services = Service.objects.all()
-        reviews = UserReview.objects.all()
-        form_data = {
-            'name': request.POST['name'],
-            'email': request.POST['email'],
-            'message': request.POST['message'],
-        }
-        email_form = EmailForm(form_data)
+    services = Service.objects.all()
+    reviews = UserReview.objects.all()
 
-        email = email_form.save()
-        customer_email = email.email
-        subject = 'Thank you for emailing us'
-        body = email.message
-
-        send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [customer_email]
-        )
-
-        context = {
-            'services': services,
-            'reviews': reviews,
-            'email_form': email_form,
-        }
-        messages.success('Your email has been sent')
-        return render(request, 'home/index.html', context)
-
-    else:
-        services = Service.objects.all()
-        reviews = UserReview.objects.all()
-        form_data = {
-            'name': 'name',
-            'email': 'email',
-            'message': 'message',
-        }
-        email_form = EmailForm(form_data)
-
-        context = {
-            'services': services,
-            'reviews': reviews,
-            'email_form': email_form,
-        }
-        return render(request, 'home/index.html', context)
+    context = {
+        'services': services,
+        'reviews': reviews,
+    }
+    return render(request, 'home/index.html', context)
